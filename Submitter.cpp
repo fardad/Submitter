bd403c02baeaa722723bca960c3c49e2561c01df
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2024-02-09 00:00:25 -0500

    (V0.99.10.0)

--- Submitter.cpp
+++ Submitter.cpp
@@ -677,7 +677,6 @@ namespace seneca {
       cout << col_yellow << "DEBUGGING SUBMITTER" << endl;
       cout << "Comment debug defines in debug.h to turn off debugging......." << col_end << endl;
 #endif
-      cout << "home: " << logpath << endl;
       cout << col_grey << "Submitter (V" << SUBMITTER_VERSION << ")" << endl;
       cout << "by Fardad S. (Last update: " << SUBMITTER_DATE << ")" << endl
          << "===============================================================" << col_end << endl <<
